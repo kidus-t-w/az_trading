3fbebfc16cebb8be6471972b7ca116ded4e92998
kidus-t-w <kidusk69@gmail.com>
2024-07-20 17:03:38 +0300

    modified card

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -2,7 +2,6 @@
 
 import { Link } from "react-router-dom";
 
-
 interface CardProps {
   name: string;
   description: string;
@@ -12,15 +11,20 @@ interface CardProps {
 
 const Card = ({ name, description, img, price }: CardProps) => {
   return (
-    <div className="max-w-sm  overflow-hidden shadow-lg h-[700px] rounded-lg rounded-tl-full rounded-tr-full grid grid-row-3 justify-between">
-      <img src={img} alt={name} className=" object-cover" />
+    <div className="max-w-sm  overflow-hidden shadow-lg h-[720px] rounded-lg rounded-tl-full rounded-tr-full flex flex-col  justify-evenly">
+      <div>
+        <img src={img} alt={name} className=" object-cover" />
+      </div>
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{name}</div>
         <p className="text-gray-700 text-base">{description}</p>
         <p className="text-gray-700 text-base">${price}</p>
       </div>
       <div className="px-6  pb-2 ">
-        <Link to='/product_detail' className="text-green-500 hover:text-green-800">
+        <Link
+          to="/product_detail"
+          className="text-green-500 hover:text-green-800"
+        >
           Read More
         </Link>
       </div>
