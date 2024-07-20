fced70fa40601a72361b4ca77ba4438800ac9b86
kidus-t-w <kidusk69@gmail.com>
2024-07-20 11:28:37 +0300

    Modified card

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -10,14 +10,14 @@ interface CardProps {
 
 const Card = ({ name, description, img, price }: CardProps) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg h-[680px] rounded-tl-full rounded-tr-full rounded-br-full rounded-bl-lg">
-      <img src={img} alt={name} className="w-full  object-cover" />
+    <div className="max-w-sm  overflow-hidden shadow-lg h-[700px] rounded-lg rounded-tl-full rounded-tr-full grid grid-row-3 justify-between">
+      <img src={img} alt={name} className="w-[300px]  object-cover" />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{name}</div>
         <p className="text-gray-700 text-base">{description}</p>
         <p className="text-gray-700 text-base">${price}</p>
       </div>
-      <div className="px-6 pt-4 pb-2 ">
+      <div className="px-6  pb-2 ">
         <a href="#" className="text-green-500 hover:text-green-800">
           Read More
         </a>
