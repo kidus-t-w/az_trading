83a3d88843755ec60e5a956238c534fb77cf17a8
kidus-t-w <kidusk69@gmail.com>
2024-08-16 12:32:33 +0300

    Removed Socialmedia icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Pin from "./components/ui/Pin";
 
 function App() {
   return (
     <div className="font-[Poppins]">
       <Header />
-        <Pin />
         <Outlet />
       <Footer />
     </div>
